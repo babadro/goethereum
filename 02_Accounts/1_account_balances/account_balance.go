d01487eb7a88f157af7c19dafb85109dfdaf52cb
babadro <babadro@gmail.com>
2022-08-18 07:09:41 +0200

    apikey

--- 02_Accounts/1_account_balances/account_balance.go
+++ 02_Accounts/1_account_balances/account_balance.go
@@ -1,1 +1,31 @@
-package __account_balances
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	apikey := os.Getenv("apikey")
+	if apikey == "" {
+		log.Fatal("empty apikey")
+	}
+
+	client, err := ethclient.Dial("https://mainnet.infura.io/v3/" + apikey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance)
+
+}
